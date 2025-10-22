9aac8e75d5c0a2bee0607fd51be56f9254f61cdd
1Tymon1 <tymonbielski@gmail.com>
2025-10-22 19:58:42 +0200

    new branch who this

--- backend/src/main/java/com/backend/mail/dto/PutMailRequest.java
+++ backend/src/main/java/com/backend/mail/dto/PutMailRequest.java
@@ -1,11 +1,6 @@
 package com.backend.mail.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-
-import java.time.LocalDate;
-
 
 public record PutMailRequest(
         @NotBlank String emailAddress,
